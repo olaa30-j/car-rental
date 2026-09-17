e880e08e49aa1d37eba8504ca2d927dfa230d9c5
olaa30-j <olaadel.967@gmail.com>
2026-09-17 09:29:03 +0300

    feat: form

--- src/Views/LoadingSpinner.tsx
+++ src/Views/LoadingSpinner.tsx
@@ -6,7 +6,7 @@ interface LoadingSpinnerProps {
 
 const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({ fullScreen = false }) => {
   return (
-    <div className={`flex justify-center items-center mx-auto ${fullScreen ? 'h-screen' : 'h-full'}`}>
+    <div className={`flex justify-center items-center m-auto ${fullScreen ? 'h-screen' : 'h-full'}`}>
       <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
     </div>
   );
