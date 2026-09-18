148aed028a6730f16d877e86958c27a4adeb310c
olaa30-j <olaadel.967@gmail.com>
2026-09-18 09:51:08 +0300

    feat: setup deploy in github

--- src/Views/NotFound.tsx
+++ src/Views/NotFound.tsx
@@ -7,7 +7,7 @@ import carImage from '../assets/notfound/404-car.png';
 
 const NotFound: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center p-4 sm:p-8 font-primary">
+    <div className="min-h-screen flex items-center justify-center font-primary">
       <div
         className="w-full max-w-6xl min-h-[400px] sm:min-h-[500px] bg-cover bg-center bg-no-repeat shadow-2xl relative overflow-hidden"
         style={{ backgroundImage: `url(${bgImage})` }}
